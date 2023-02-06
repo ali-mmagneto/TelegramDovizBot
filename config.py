5328a644455cfdd81f777e938d18d712868bba97
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-07 00:16:32 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+import time
+
+import pyrogram
+from pyrogram import Client, enums
+
+APP_ID = os.environ.get("APP_ID", None)
+API_HASH = os.environ.get("API_HASH", None)
+BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+OWNER_ID = os.environ.get("OWNER_ID", '1276627253') 
+    
