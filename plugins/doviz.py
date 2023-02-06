4dfa2a8c2c33bfaf17e48920a8c06febfc1754a4
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-07 00:50:33 +0300

    Update doviz.py

--- plugins/doviz.py
+++ plugins/doviz.py
@@ -1,1 +1,22 @@
 # Coded By :d
+
+from pyrogram import Client, filters
+
+
+dovizurl = "https://api.genelpara.com/embed/doviz.json"
+
+@Client.on_message(filters.command('doviz'))
+async def dovizcek(bot, message):
+    try:
+        distek = requests.get(dovizurl)
+        dveri = distek.json()
+        dsonuc = dveri['USD']
+        text = f"Dolar:\nAlış: {dsonuc['alis']}\nSatış: {dsonuc['satis']}
+        await bot.send_message(
+            chat_id=message.chat.id,
+            text=text)
+    except Exception as e:
+        print(e)
+        await bot.send_message(
+        chat_id=message.chat.id,
+        text=f"`{e}`") 
