c6f508f3ec56d138574bc7351296d8b9a38a831d
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-07 01:07:31 +0300

    Update doviz.py

--- plugins/doviz.py
+++ plugins/doviz.py
@@ -14,7 +14,7 @@ async def dovizcek(bot, message):
         eistek = requests.get(dovizurl)
         everi = eistek.json()
         esonuc = everi['EUR']
-        text = f"Dolar:\nAlış: `{dsonuc['alis']}`\nSatış: `{dsonuc['satis']}`\n\Euro:\nAlış: `{esonuc['alis']}`\nSatış: `{esonuc['satis']}`"
+        text = f"Dolar:\nAlış: `{dsonuc['alis']}`\nSatış: `{dsonuc['satis']}`\n\nEuro:\nAlış: `{esonuc['alis']}`\nSatış: `{esonuc['satis']}`"
         await bot.send_message(
             chat_id=message.chat.id,
             text=text)
