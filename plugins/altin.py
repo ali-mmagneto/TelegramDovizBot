3e90401229fa3b20dd55f60a33d1de80557375f4
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-07 01:13:53 +0300

    Update altin.py

--- plugins/altin.py
+++ plugins/altin.py
@@ -1,1 +1,25 @@
 # Coded by :d
+
+from pyrogram import Client, filters
+import requests
+
+dovizurl = "https://api.genelpara.com/embed/doviz.json"
+
+@Client.on_message(filters.command('altin'))
+async def altincek(bot, message):
+    try:
+        aistek = requests.get(dovizurl)
+        averi = aistek.json()
+        asonuc = averi['C']
+        gistek = requests.get(dovizurl)
+        gveri = gistek.json()
+        gsonuc = gveri['GA']
+        text = f"Çeyrek Altın:\nAlış: `{asonuc['alis']}`\nSatış: `{asonuc['satis']}`\n\nGram Altın:\nAlış: `{gsonuc['alis']}`\nSatış: `{gsonuc['satis']}`"
+        await bot.send_message(
+            chat_id=message.chat.id,
+            text=text)
+    except Exception as e:
+        print(e)
+        await bot.send_message(
+        chat_id=message.chat.id,
+        text=f"`{e}`")
