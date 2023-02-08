8ee103c631c60dbd26c632abe56cd1c9e372749e
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-08 21:49:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ from pyrogram import Client, enums
 APP_ID = os.environ.get("APP_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-OWNER_ID = os.environ.get("OWNER_ID", '1276627253') 
+OWNER_ID = os.environ.get("OWNER_ID", 'mmagneto') # Telegram kullanıcı adı girin.
     
